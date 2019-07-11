1240a92d91c47182619058b87cfbed8d861336ad
JCTGY <fan3510@gmail.com>
2019-07-10 20:50:03 -0700

    add namespace std

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -6,27 +6,28 @@
 /*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 15:06:54 by jchiang-          #+#    #+#             */
-/*   Updated: 2019/07/07 15:44:19 by jchiang-         ###   ########.fr       */
+/*   Updated: 2019/07/10 20:49:14 by jchiang-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+using namespace std;
 
 void	big_case(char *str){
 	for (int i = 0; str[i]; i++){
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
-		std::cout << str[i];
+		cout << str[i];
 	}
 }
 int		main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else {
 		for (int i = 1; i <  argc; i++)
 			big_case(argv[i]);
 	}
-	std::cout << std::endl;
+	cout << std::endl;
 	return 0;
 }
