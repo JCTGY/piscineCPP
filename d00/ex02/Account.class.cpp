fab27473678a601c5e038429214e2737c4daf38f
JCTGY <fan3510@gmail.com>
2019-10-22 15:30:18 -0700

    add d00 ex02

---
+++ d00/ex02/Account.class.cpp
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.class.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 09:56:53 by jchiang-          #+#    #+#             */
+/*   Updated: 2019/10/22 15:26:40 by jchiang-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.class.hpp"
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(void) {
+
+}
+
+Account::Account(int initial_deposit) {
+	
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << initial_deposit << ";created" <<  std::endl;
+}
+
+Account::~Account(void) {
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount
+		<< ";closed" <<std::endl;
+}
+
+void Account::makeDeposit(int deposit) {
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount 
+		<< ";deposits:" << deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_amount += deposit;
+	_totalAmount += deposit;
+	std::cout << ";amount:" << _amount << ";np_deposits:" << _nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+	
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+	if (_amount >= withdrawal){
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		std::cout << ";withdrawal:" << withdrawal << ";amount:" << _amount
+			<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+		return true;
+	}
+	else {
+		std::cout << ";withdrawal:refused" << std::endl;
+		return false;
+	}
+}
+
+int Account::checkAmount(void) const {
+	return (_amount);
+
+}
+
+void Account::displayStatus(void) const {
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount 
+		<< ";deposits:" <<  _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl; 
+}
+
+
+int Account::getNbAccounts(void) {
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount(void) {
+	return (_totalAmount);
+}
+
+int Account::getNbDeposits(void) {
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void) {
+	return (_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos(void) {
+
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";totla:" <<
+	   	_totalAmount << ";deposits:" << _totalNbDeposits <<
+	   	";withdrawls:" << _totalNbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp(void) {
+
+	std::time_t t = std::time(nullptr);
+	std::cout << std::put_time(std::localtime(&t), "[%Y%m%d_%H%M%S]");
+}
+
---
+++ d01/ex04/ex04.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex04.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 08:38:47 by jchiang-          #+#    #+#             */
+/*   Updated: 2019/10/22 08:46:50 by jchiang-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string theString = "HI THIS IS BRAIN";
+	std::string *pointer = &theString;
+	std::string &refer = theString;
+
+	std::cout << "pointer" << *pointer << std::endl;
+	std::cout << "refer" << refer << std::endl;
+
+	return 0;
+}
